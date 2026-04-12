5e90d3c65c57f2f11efa27d73e1e9f510e7afdc5
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2026-04-12 09:14:44 +0100

    Switch to Lychee

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
           buildInputs = [dotnet-sdk];
           packages = [
             pkgs.alejandra
-            pkgs.nodePackages.markdown-link-check
+            pkgs.lychee
             pkgs.shellcheck
             pkgs.xmlstarlet
           ];
